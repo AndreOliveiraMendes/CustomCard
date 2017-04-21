984cd4d1323e0e0247827bfbb8fd283601c955fb
AndreOliveiraMendes <ao_mendes@hotmail.com>
2017-04-21 01:27:40 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 Yuudachi={}
 poi=Yuudachi
+--function to collect info
 Card.info=function (c)
 	local code=c:GetCode()
 	local t=c:GetType()
@@ -77,91 +78,7 @@ function Yuudachi.BeginPuzzle(otp,skdp,sksp,effect)
 		Duel.RegisterEffect(e3,0)
 	end
 end
---atempt to make custom synchro
-function Yuudachi.AddCustomSynchro(c,f1,f2,ct)
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_SPSUMMON_PROC)
-	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_IGNORE_IMMUNE)
-	e1:SetRange(LOCATION_EXTRA)
-	e1:SetCondition(Yuudachi.SynCondition(f1,f2,ct,99))
-	e1:SetTarget(Yuudachi.SynTarget(f1,f2,ct,99))
-	e1:SetOperation(Yuudachi.SynOperation(f1,f2,ct,99))
-	e1:SetValue(SUMMON_TYPE_SYNCHRO)
-	c:RegisterEffect(e1)
-end
-function Yuudachi.SynFilter1(c)
-	return c:IsType(TYPE_TUNER) and (not f1 or f1(c))
-end
-function Yuudachi.SynFilter2(c,mc)
-	return not c:IsType(TYPE_TUNER) and (not f2 or f2(c)) and c:IsCanBeSynchroMaterial(mc)
-end
-function Yuudachi.SynCondition(f1,f2,minct,maxc)
-	return  function(e,c)
-				if c==nil then return true end
-				if c:IsType(TYPE_PENDULUM) and c:IsFaceup() then return false end
-				local ft=Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)
-				local ct=-ft
-				local minc=minct
-				if minc<ct then minc=ct end
-				if maxc<minc then return false end
-				local mg=Duel.GetFieldGroup(c:GetControler(),LOCATION_MZONE,0)
-				local tmat=mg:Filter(Yuudachi.SynFilter1,nil,c,f1)
-				local smat=mg:Filter(Yuudachi.SynFilter2,nil,c,f2)
-				local tuner=tmat:GetFirst()
-				local check=false
-				while tuner do
-					if tuner:GetLevel()~=0 and smat:CheckWithSumEqual(Card.GetSynchroLevel,c:GetLevel()*tuner:GetLevel(),minc,maxc,c) then
-						check=true
-					end
-					tuner=tmat:GetNext()
-				end
-				return check
-			end
-end
-function Yuudachi.SynTarget(f1,f2,minct,maxc)
-	return  function(e,tp,eg,ep,ev,re,r,rp,chk,c)
-				local g=Group.CreateGroup()
-				local ft=Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)
-				local ct=-ft
-				local minc=minct
-				if minc<ct then minc=ct end
-				local mg=Duel.GetFieldGroup(c:GetControler(),LOCATION_MZONE,0)
-				local tmat=mg:Filter(Yuudachi.SynFilter1,nil,c,f1)
-				local smat=mg:Filter(Yuudachi.SynFilter2,nil,c,f2)
-				local tuner=tmat:GetFirst()
-				while tuner do
-					if tuner:GetLevel()==0 or not smat:CheckWithSumEqual(Card.GetSynchroLevel,c:GetLevel()*tuner:GetLevel(),minc,maxc,c) then
-						tmat:RemoveCard(tuner)
-					end
-					tuner=tmat:GetNext()
-				end
-				if tmat:GetCount()>1 then
-					tmat:Select(c:GetControler(),1,1,nil)
-				end
-				tuner=tmat:GetFirst()
-				g:AddCard(tuner)
-				smat=smat:SelectWithSumEqual(c:GetControler(),Card.GetSynchroLevel,c:GetLevel()*tuner:GetLevel(),minc,maxc,c)
-				atuner=smat:GetFirst()
-				while atuner do
-					g:AddCard(atuner)
-					atuner=smat:GetNext()
-				end
-				if g then
-					g:KeepAlive()
-					e:SetLabelObject(g)
-					return true
-				else return false end
-			end
-end
-function Yuudachi.SynOperation(f1,f2,minct,maxc)
-	return  function(e,tp,eg,ep,ev,re,r,rp,c)
-				local g=e:GetLabelObject()
-				c:SetMaterial(g)
-				Duel.SendtoGrave(g,REASON_MATERIAL+REASON_SYNCHRO)
-				g:DeleteGroup()
-			end
-end
+--custom function
 function Yuudachi.dm(a,d,lp,tp)
     if not d then
         if a:IsPosition(POS_FACEUP_ATTACK) and a:IsControler(1-tp) then
